7424339f50d0e0fabcddd34a3989f798dd3486a1
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-02 19:06:14 -0700

    Create pinpoint.js
    Will add estimation function later

---
+++ pinpoint.js
@@ -0,0 +1,5 @@
+function pinpoint(canvas, location, color, size) {
+    document.getElementById(canvas).getContext("2d").fillStyle = `rgb(${color.br ? (color.br * 255) : color.rg ? (255 - color.rg * 255) : color.r}, ${color.rg ? (color.rg * 255) : color.gb ? (255 - color.gb * 255) : color.g}, ${color.gb ? (color.gb * 255) : color.br ? (255 - color.br * 255) : color.b})`;
+    document.getElementById(canvas).fillRect(location.x * size, location.y * size, size, size);
+}
+// You can easily configure a function to the above using your preferred settings, but hue combos other than these might not work as intended: R G B, R↔G B, G↔B R, B↔R G
