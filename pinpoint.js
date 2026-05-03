29c2b145ba91a1df95e69da98040c7e827f26f74
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-03 12:39:44 -0700

    Update pinpoint.js
    correct branch

--- pinpoint.js
+++ pinpoint.js
@@ -2,7 +2,7 @@ pinpoint_graph = {
     "points": [],
     "pinpoint": function(canvas, location, color, size) {
         document.getElementById(canvas).getContext("2d").fillStyle = `rgb(${color.br ? (color.br * 255) : color.rg ? (255 - color.rg * 255) : color.r}, ${color.rg ? (color.rg * 255) : color.gb ? (255 - color.gb * 255) : color.g}, ${color.gb ? (color.gb * 255) : color.br ? (255 - color.br * 255) : color.b})`;
-        document.getElementById(canvas).fillRect(location.x * size, location.y * size, size, size);
+        document.getElementById(canvas).getContext("2d").fillRect(location.x * size, location.y * size, size, size);
         pinpoint_graph.points.push({
             "location": location,
             "color": color
