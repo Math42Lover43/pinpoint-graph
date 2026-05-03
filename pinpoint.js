24601d5f5f7e8d596f1d81f6da5719da4b18c8cc
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-03 10:14:40 -0700

    Update pinpoint.js
    put in an object to not be invasive

--- pinpoint.js
+++ pinpoint.js
@@ -1,5 +1,13 @@
-function pinpoint(canvas, location, color, size) {
-    document.getElementById(canvas).getContext("2d").fillStyle = `rgb(${color.br ? (color.br * 255) : color.rg ? (255 - color.rg * 255) : color.r}, ${color.rg ? (color.rg * 255) : color.gb ? (255 - color.gb * 255) : color.g}, ${color.gb ? (color.gb * 255) : color.br ? (255 - color.br * 255) : color.b})`;
-    document.getElementById(canvas).fillRect(location.x * size, location.y * size, size, size);
+pinpoint_graph = {
+    "points": [];
+    "pinpoint": function(canvas, location, color, size) {
+        document.getElementById(canvas).getContext("2d").fillStyle = `rgb(${color.br ? (color.br * 255) : color.rg ? (255 - color.rg * 255) : color.r}, ${color.rg ? (color.rg * 255) : color.gb ? (255 - color.gb * 255) : color.g}, ${color.gb ? (color.gb * 255) : color.br ? (255 - color.br * 255) : color.b})`;
+        document.getElementById(canvas).fillRect(location.x * size, location.y * size, size, size);
+        pinpoint_graph.points.push({
+            "location": location,
+            "color": color
+        });
+    }
 }
-// You can easily configure a function to the above using your preferred settings, but hue combos other than these might not work as intended: R G B, R↔G B, G↔B R, B↔R G
+// You can easily configure a function to pinpoint_graph.pinpoint using your preferred settings, but hue combos other than these might not work as intended: R G B, R↔G B, G↔B R, B↔R G
+// If you want to create multiple graphs using this extension, then be sure to reset pinpoint_graph.points to [];
