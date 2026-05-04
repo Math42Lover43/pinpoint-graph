b271ab131047e64cb7510814f3f9e5b450ecbe29
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-03 20:51:36 -0700

    Update pinpoint.js
    Added function to determine what points should be averaged

--- pinpoint.js
+++ pinpoint.js
@@ -15,6 +15,7 @@ pinpoint_graph = {
         var change = Infinity;
         var dist = 3;
         var vectors;
+        var relationships = [];
         while(change != 0 || dist <= pinpoint_graph.points.length) {
             change = 0;
             dist++;
@@ -25,9 +26,13 @@ pinpoint_graph = {
                 }
             }
             console.log(dist, vectors);
+            for(let point = 0; point < pinpoint_graph.points.length; point++) {
+                relationships.push([pinpoint_graph.points[point]].concat(pinpoint_graph.filter(i => (i.x - pinpoint_graph.points[point].x) ** 2 + (i.y - pinpoint_graph.points[point].y) ** 2 == dist ** 2)))
+            }
+            console.log(relationships);
         }
     }
 }
 // You can easily configure a function to pinpoint_graph.pinpoint using your preferred settings, but hue combos other than these might not work as intended: R G B, R↔G B, G↔B R, B↔R G
 // If you want to create multiple graphs using this extension, then be sure to reset pinpoint_graph.points to [];
-// For one graph, plot all the points you want first, then use the interpolate function. Please use one pixel size and one target canvas, otherwise, you may get bad results.
+// For one graph, plot all the points you want first, then use the interpolate function. Please use one pixel size and one target canvas throughout, otherwise, you may get bad results.
