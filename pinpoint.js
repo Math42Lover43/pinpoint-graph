ceebcfc34281927ec2a28c5a6316789845662656
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-04 20:25:29 -0700

    Update pinpoint.js
    is this a better way to check for length?

--- pinpoint.js
+++ pinpoint.js
@@ -58,8 +58,8 @@ pinpoint_graph = {
             candidates = candidates.filter(x => x);
             console.log(dist, candidates);
             for(let cand = 0; cand < candidates.length; cand++) {
-                console.log(pinpoint_graph.points.filter(x => (x.location == candidates[cand].location)));
-                if(pinpoint_graph.points.filter(x => (x.location == candidates[cand].location)) == []) {
+                console.log(pinpoint_graph.points.filter(x => (x.location == candidates[cand].location)).length);
+                if(!pinpoint_graph.points.filter(x => (x.location == candidates[cand].location)).length) {
                     pinpoint_graph.pinpoint(canvas, candidates[cand].location, candidates[cand].color, pinpoint_graph.size);
                     change++;
                 }
