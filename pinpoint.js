53ec30eecc7ea3d61c729731a95ed404cae5fd9b
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-05 15:57:09 -0700

    Update pinpoint.js
    ${}

--- pinpoint.js
+++ pinpoint.js
@@ -54,7 +54,7 @@ pinpoint_graph = {
                         var sqmag = (x[1].location.x - x[0].location.x) ** 2 + (x[1].location.y - x[0].location.y) ** 2;
                         var rate = {"location": {"x": (x[1].location.x - x[0].location.x) / sqmag, "y": (x[1].location.y - x[0].location.y) / sqmag}, "color": {"r": (x[1].color.r - x[0].color.r) / sqmag, "g": (x[1].color.g - x[0].color.g) / sqmag, "b": (x[1].color.b - x[0].color.b) / sqmag, "rg": (x[1].color.rg - x[0].color.rg) / sqmag, "gb": (x[1].color.gb - x[0].color.gb) / sqmag, "br": (x[1].color.br - x[0].color.br) / sqmag}};
                         for(let prop = 0; prop < props.length; prop++) {
-                            eval(`rate{props[prop]} = rate{props[prop]} ? rate{props[prop]} : 0;`);
+                            eval(`rate${props[prop]} = rate${props[prop]} ? rate${props[prop]} : 0;`);
                         }
                         var point = x[0];
                         for(let weight = 0; weight < sqmag - 2; weight++) {
