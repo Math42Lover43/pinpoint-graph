e5cc6414e0e9d821f6e40d48131e5b5602cda415
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-06 07:31:20 -0700

    Update pinpoint.js
    log coordinates to see mismatches

--- pinpoint.js
+++ pinpoint.js
@@ -115,7 +115,11 @@ pinpoint_graph = {
                 });
                 console.log(candidates);
                 // x => x && (pinpoint_graph.points.filter(k => Math.round(1000000 * k.location.x) == Math.round(1000000 * x.location.x) && Math.round(1000000 * k.location.y) == Math.round(1000000 * x.location.y)).length == 0)
-                candidates = candidates.filter(x => x && (pinpoint_graph.points.filter(k => Math.round(1000000 * k.location.x) == Math.round(1000000 * x.location.x) && Math.round(1000000 * k.location.y) == Math.round(1000000 * x.location.y)).length == 0));
+                candidates = candidates.filter(function(x) {
+                    pinpoint_graph.points.filter(function(k) {
+                        console.log(k, x, k.location.x, x.location.x, k.location.y, x.location.y);
+                    });
+                });
                 console.log(dist, candidates);
                 for(let cand = 0; cand < candidates.length; cand++) {
                     pinpoint_graph.pinpoint(canvas, candidates[cand].location, candidates[cand].color, pinpoint_graph.size);
