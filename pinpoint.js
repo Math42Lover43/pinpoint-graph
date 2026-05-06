57159a8259b199a5bc62cdffe90fe0ec3b7dd137
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-05 22:21:09 -0700

    Update pinpoint.js
    is everything reasonable? I think I may need to rewrite this case entirely

--- pinpoint.js
+++ pinpoint.js
@@ -77,6 +77,7 @@ pinpoint_graph = {
                         var vect = pinpoint_graph.vectorred([x[1].location.x - x[0].location.x, x[1].location.y - x[0].location.y])
                         var sqmag = vect.coefficient;
                         var rate = {"location": {"x": vect.vector[0], "y": vect.vector[1]}, "color": {"r": (x[1].color.r - x[0].color.r) / sqmag, "g": (x[1].color.g - x[0].color.g) / sqmag, "b": (x[1].color.b - x[0].color.b) / sqmag, "rg": (x[1].color.rg - x[0].color.rg) / sqmag, "gb": (x[1].color.gb - x[0].color.gb) / sqmag, "br": (x[1].color.br - x[0].color.br) / sqmag}};
+                        console.log(x, rate);
                         for(let prop = 0; prop < props.length; prop++) {
                             eval(`
                                 rate${props[prop]} = rate${props[prop]} ? rate${props[prop]} : 0;
