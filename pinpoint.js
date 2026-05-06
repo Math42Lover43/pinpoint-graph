9f61381a0b977cf260600e0d832ac664593a3fc3
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-06 09:06:58 -0700

    Update pinpoint.js
    what is the type of x?

--- pinpoint.js
+++ pinpoint.js
@@ -109,7 +109,7 @@ pinpoint_graph = {
                 });
                 console.log(candidates);
                 // x => x && pinpoint_graph.points.filter(k => k.location.x == x.location.x && k.location.y == x.location.y).length == 0)
-                candidates = candidates.filter(x => x && (pinpoint_graph.points.filter(k => k.location.x == x.location.x && k.location.y == x.location.y).length == 0));
+                candidates = candidates.filter(x => console.log(x); && (pinpoint_graph.points.filter(k => k.location.x == x.location.x && k.location.y == x.location.y).length == 0));
                 console.log(dist, candidates);
                 for(let cand = 0; cand < candidates.length; cand++) {
                     pinpoint_graph.pinpoint(canvas, candidates[cand].location, candidates[cand].color, pinpoint_graph.size);
