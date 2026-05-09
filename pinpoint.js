fed2a81ddc3c7a4b7256c7eed83843322802ab76
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-08 19:24:08 -0700

    Update pinpoint.js
    suffocated points die to avoid overloading, and they are unimportant since they do not need to be involved in interpolation anymore.

--- pinpoint.js
+++ pinpoint.js
@@ -41,6 +41,7 @@ pinpoint_graph = {
             var relationships;
             var siblings;
             var candidates;
+            var oxygen;
             var props = [
                 ".location.x",
                 ".location.y",
@@ -63,6 +64,18 @@ pinpoint_graph = {
                 }
                 change = 0;
                 relationships = [];
+                pinpoint_graph = pinpoint_graph.points.filter(function(x) {
+                    var unblock = 8;
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == -1 && (y.location.y - x.location.y) == -1);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == -1 && (y.location.y - x.location.y) == 0);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == -1 && (y.location.y - x.location.y) == 1);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == 0 && (y.location.y - x.location.y) == -1);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == 0 && (y.location.y - x.location.y) == 1);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == 1 && (y.location.y - x.location.y) == -1);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == 1 && (y.location.y - x.location.y) == 0);
+                    unblock -= pinpoint_graph.points.find(y => (y.location.x - x.location.x) == 1 && (y.location.y - x.location.y) == 1);
+                    return unblock;
+                })
                 for(let point = 0; point < pinpoint_graph.points.length; point++) {
                     // siblings = pinpoint_graph.points.filter(i => (i.location.x - pinpoint_graph.points[point].location.x) ** 2 + (i.location.y - pinpoint_graph.points[point].location.y) ** 2 == dist);
                     siblings = pinpoint_graph.points.filter(i => (i.location.x - pinpoint_graph.points[point].location.x) ** 2 + (i.location.y - pinpoint_graph.points[point].location.y) ** 2 == dist);
