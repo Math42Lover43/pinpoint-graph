f603f852c6fb5f1ee70b6ce5dfa241a9dfa23d72
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2026-05-09 12:49:05 -0700

    Update pinpoint.js
    return to previous

--- pinpoint.js
+++ pinpoint.js
@@ -42,7 +42,6 @@ pinpoint_graph = {
             var relationships;
             var siblings;
             var candidates;
-            var oxygen = [];
             var props = [
                 ".location.x",
                 ".location.y",
@@ -69,15 +68,7 @@ pinpoint_graph = {
                     // siblings = pinpoint_graph.points.filter(i => (i.location.x - pinpoint_graph.points[point].location.x) ** 2 + (i.location.y - pinpoint_graph.points[point].location.y) ** 2 == pinpoint_graph.vectors_allowed[dist]);
                     siblings = pinpoint_graph.points.filter(i => (i.location.x - pinpoint_graph.points[point].location.x) ** 2 + (i.location.y - pinpoint_graph.points[point].location.y) ** 2 == pinpoint_graph.vectors_allowed[dist]);
                     siblings = siblings.map(x => structuredClone(x));
-                    if(siblings.length > 2) {
-                        relationships.push([structuredClone(pinpoint_graph.points[point])].concat(siblings));
-                    }
-                    for(let sis = 0; sis < siblings.length; sis++) {
-                        if(!oxygen.includes(`${pinpoint_graph.points[point].location.x} ${pinpoint_graph.points[point].location.y} ${siblings[sis].location.x} ${siblings[sis].location.y}`)) {
-                            relationships.push([structuredClone(pinpoint_graph.points[point]), siblings[sis]]);
-                            oxygen.push(`${pinpoint_graph.points[point].location.x} ${pinpoint_graph.points[point].location.y} ${siblings[sis].location.x} ${siblings[sis].location.y}`);
-                        }
-                    }
+                    relationships.push([structuredClone(pinpoint_graph.points[point])].concat(siblings));
                 }
                 relationships = relationships.sort((a, b) => -(a.length - b.length));
                 console.log(relationships);
